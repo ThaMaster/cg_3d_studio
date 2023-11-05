42c6da333525352adb58a3db9f52f73eeb887bb2
Christoffer Nordlander <christoffernordlnader02@gmail.com>
2023-11-05 16:01:00 +0100

    Changed name of main to matrixRoutinesTest. Removed some tests for the object loader in the matrixRoutinesTest file.

--- matrixRoutinesTest.cpp
+++ matrixRoutinesTest.cpp
@@ -29,31 +29,7 @@ int main () {
     std::cout << "Rotate-X: " << rotatex(45 *(M_PI/180)) << std::endl;
     std::cout << "Sheer-XZ: " << shear(45 *(M_PI/180), 'x', 'z') << std::endl;
     std::cout << "Prod of translation * scale: " << prod(translate(1,2,3), scale(1,2,3)) << std::endl;
-    std::cout << "Prod of scale * translation: " << prod(scale(1,2,3), translate(1,2,3)) << std::endl;
-
-    // Create a Loader Object
-    std::string inputFile = "teddy.obj";
-    tinyobj::ObjReaderConfig reader_config;
-    reader_config.mtl_search_path = "./";
-    tinyobj::ObjReader reader;
-
-    if(!reader.ParseFromFile(inputFile, reader_config)) {
-        if(!reader.Error().empty()) {
-            std::cerr << "TinyObjReader: " << reader.Error();
-        }
-        exit(EXIT_FAILURE);
-    }
-    
-    if (!reader.Warning().empty()) {
-        std::cout << "TinyObjReader: " << reader.Warning();
-    }
-
-    auto& attrib = reader.GetAttrib();
-    auto& shapes = reader.GetShapes();
-    for(size_t i = 0; i < shapes[0].mesh.num_face_vertices.size(); i++) {
-        std::cout << attrib.vertices[0] << std::endl;
-    }
-    
+    std::cout << "Prod of scale * translation: " << prod(scale(1,2,3), translate(1,2,3)) << std::endl;    
     return 0;
 }
 
