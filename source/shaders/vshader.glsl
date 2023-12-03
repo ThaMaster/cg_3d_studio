fc588cc0221d146974d67f8d508bf273110dd3a8
Christoffer Nordlander <christoffernordlander02@gmail.com>
2023-12-03 12:27:41 +0100

    Think the lightning calculations now work

--- source/shaders/vshader.glsl
+++ source/shaders/vshader.glsl
@@ -14,23 +14,26 @@ vec4 diffuse;
 vec4 specular;
 
 vec4 Ia = vec4( 0.1, 0.1, 0.1, 1.0 );
-vec4 Il = vec4( 0.4, 0.1, 0.2, 1.0 );
+vec4 Il = vec4( 0.4, 0.4, 0.2, 1.0 );
 vec4 ka = vec4( 1.0, 1.0, 1.0, 1.0 );
 vec4 kd = ka;
 vec4 ks = kd;
-vec4 l = vec4( 2.0, 0.5, 5.0, 0.0 );
-vec4 v = vec4( 0.0, 0.0, 2.0, 0.0 );
+vec4 l = normalize(vec4( 2.0, 0.5, 5.0, 0.0 ));
+vec4 v = normalize(vec4( 0.0, 0.0, 2.0, 0.0 ));
 
 void
 main()
 {
     ambient = Ia * ka;
+
+    float diffIntensity = max(dot(normalize(vec4(vNormal, 1.0)), l), 0);
     
-    if(dot( vec4( normalize( vNormal ), 0 ), l) > 0 ) {
-        
-        diffuse = Il * kd * max( dot( vec4( normalize( vNormal ), 0 ), l ) , 0 );
-        vec4 r = normalize( reflect( l, vec4( normalize( vNormal ), 0 ) ) );
-        specular = Il * ks * pow( max( dot( r, v ), 0 ), 2 );
+    if(diffIntensity > 0.0) {
+
+        diffuse = Il * kd * diffIntensity;        
+        vec4 r = normalize(reflect( -l, normalize(vec4( vNormal , 0.0 )) ));
+        float specIntensity = pow( max( dot( v, r ), 0 ),  2);
+        specular = Il * ks * specIntensity;
 
         color = ambient + diffuse + specular;
 
@@ -40,9 +43,5 @@ main()
 
     }
         
-
-
-    
-
     gl_Position =  P * V * M * vec4( vPosition, 1.0 );
 }
